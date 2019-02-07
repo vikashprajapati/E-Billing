c8391d83084327265f03785958f10ea1f04e2f74
vikashprajapati <prajapativikash0000@gmail.com>
2019-02-07 22:56:48 +0530

    initialized project

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include_once('resources/views/header.php') ?>
+    <title>Tile goes here</title>
+</head>
+<body class="bg-grey-lighter h-screen flex flex-col">
+    <?php include_once('resources/views/navbar.php') ?>
+
+    <main class="flex-1 mx-5">
+        <h1 class="font-monospace text-orange px-4 my-4">Homepage</h1>
+        <div class="content flex my-5">
+            <div class="main-content w-3/4 bg-white h-screen">
+
+            </div>
+            <div class="sidebar w-1/4 bg-white mx-1 h-screen">
+                
+            </div>
+        </div>
+    </main>
+
+    <?php include_once('resources/views/footer.php') ?>
+</body>
+</html>
---
+++ resources/views/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="flex justify-center bg-grey-darkest text-white py-2">
+        <ul class="list-reset flex">
+            <li class="px-1">facebook</li>
+            <li class="px-1">Instagram</li>
+            <li class="px-1">twitter</li>
+        </ul>
+    </div>
+</footer>
