f3b66d4a0ac5f9a5c40beece028b27f1c95e7f33
vikashprajapati <prajapativikash0000@gmail.com>
2019-02-08 21:28:34 +0530

    basic homepage added

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php include_once('resources/views/header.php') ?>
-    <title>Tile goes here</title>
+    <title>Ebilling</title>
 </head>
 <body class="bg-grey-lighter h-screen flex flex-col">
     <?php include_once('resources/views/navbar.php') ?>
-
-    <main class="flex-1 mx-5">
-        <h1 class="font-monospace text-orange px-4 my-4">Homepage</h1>
-        <div class="content flex my-5">
-            <div class="main-content w-3/4  h-screen">
-                <?php foreach (range(1,5) as $i) ?>
-                {
-                    <div class="card min-h-20 bg-white rounded flex flex-1 flex-col p-4">
+    <main class="flex flex-1 flex-col m-5">
+        <h2 class="font-monospace text-orange-dark px-4 my-5">Current Day Bills</h2>
+        <div class="content flex flex-1 ">
+            <div class="main-content w-3/4 mx-1">
+                <?php foreach(range(1,15) as $i): ?>
+                    <div class="card min-h-20 my-5 bg-white rounded flex flex-1 flex-col p-4">
                         <div class="card-header ">
-                            <h3 class="text-orange-dark py-2">Customer-Name  <span class="text-grey-darkest float-right"><strong>issued-by: </strong></span></h3>
+                            <h3 class="text-orange-dark py-2"> <a href="#"> Customer-Name</a><small class="text-grey-darkest font-light text-base float-right">issued-by:</small></h3>
                         </div>
                         <div class="card-content">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, vel?
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                         </div>
                     </div>
-                }
-                <?php endforeach ?> 
+                <?php endforeach ?>
             </div>
-            <div class="sidebar w-1/4 mx-1 h-screen">
-                <?php include_once('resources/views/navbar.php') ?>
+            <div class="sidebar w-1/4 mx-1 flex flex-col">
+                <?php include_once('resources/views/sidebar.php') ?>
             </div>
         </div>
     </main>
--- resources/views/footer.php
+++ resources/views/footer.php
@@ -1,9 +1,5 @@
 <footer>
     <div class="flex justify-center bg-grey-darkest text-white py-2">
-        <ul class="list-reset flex">
-            <li class="px-1">facebook</li>
-            <li class="px-1">Instagram</li>
-            <li class="px-1">twitter</li>
-        </ul>
+        <p>copyright Ebilling.com &copy; 2019, all right reserved.</p>
     </div>
 </footer>
